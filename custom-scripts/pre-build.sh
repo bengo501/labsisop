0254e1492bd079f123b51f6010927450fd2d753b
Bernardo K. Heitz <72239771+bengo501@users.noreply.github.com>
2025-03-30 14:23:36 +0000

    Tutorial 2 - Configuração de rede e SSH com Dropbear concluídos

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -1,5 +1,9 @@
-
 #!/bin/sh
 
-cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
-chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+# Caminho completo para o diretório alvo
+TARGET_DIR=$1
+
+# Copiar o script para o diretório correto
+cp ../custom-scripts/S41network-config $TARGET_DIR/etc/init.d/
+chmod +x $TARGET_DIR/etc/init.d/S41network-config
+

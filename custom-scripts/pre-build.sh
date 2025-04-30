d0cff442495e3d38826b3f4fbfc694e3268127a2
Bernardo K. Heitz <72239771+bengo501@users.noreply.github.com>
2025-04-30 20:43:52 +0000

    Pending changes exported from your codespace

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -4,6 +4,14 @@
 TARGET_DIR=$1
 
 # Copiar o script para o diretório correto
-cp ../custom-scripts/S41network-config $TARGET_DIR/etc/init.d/
-chmod +x $TARGET_DIR/etc/init.d/S41network-config
+cp ../custom-scripts/S41network-config $TARGET_DIR/target/etc/init.d/
+chmod +x $TARGET_DIR/target/etc/init.d/S41network-config
+
+cp ../custom-scripts/S50hello $TARGET_DIR/target/etc/init.d/
+chmod +x $TARGET_DIR/target/etc/init.d/S50hello
+
+cp board/labsisop/linuxstatus.py output/target/usr/bin/
+chmod +x output/target/usr/bin/linuxstatus.py
+
+chmod +x output/target/etc/init.d/S50linuxstatus
 
